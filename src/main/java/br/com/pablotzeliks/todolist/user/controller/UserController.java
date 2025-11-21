7f83bffb13da88d007c1b2d11a9d7f90b6171dbc
PabloTzeliks <arq.pabloo@gmail.com>
2025-11-21 12:52:09 -0300

    feat: add the initial structure for the user

---
+++ src/main/java/br/com/pablotzeliks/todolist/user/controller/UserController.java
@@ -0,0 +1,17 @@
+package br.com.pablotzeliks.todolist.user.controller;
+
+import br.com.pablotzeliks.todolist.user.model.User;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    // Endpoint to create a new user
+    public void create(@RequestBody User user) {
+
+
+    }
+}
