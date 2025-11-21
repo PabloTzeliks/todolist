75e69fb33938604da5fbf1a074741e43bef6d81f
PabloTzeliks <arq.pabloo@gmail.com>
2025-11-21 13:19:34 -0300

    feat: implement the first route for create a user

--- src/main/java/br/com/pablotzeliks/todolist/user/controller/UserController.java
+++ src/main/java/br/com/pablotzeliks/todolist/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package br.com.pablotzeliks.todolist.user.controller;
 
 import br.com.pablotzeliks.todolist.user.model.User;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     // Endpoint to create a new user
+
+    @PostMapping("/create")
     public void create(@RequestBody User user) {
 
-
+        System.out.println("Creating user: " + user.getUsername());
     }
 }
