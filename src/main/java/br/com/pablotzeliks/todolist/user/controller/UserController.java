5cb03bfb7ad6fee71e1952ef160ead21a7e9bdab
PabloTzeliks <arq.pabloo@gmail.com>
2025-11-22 15:40:15 -0300

    feat: implement raw user creation feature

--- src/main/java/br/com/pablotzeliks/todolist/user/controller/UserController.java
+++ src/main/java/br/com/pablotzeliks/todolist/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package br.com.pablotzeliks.todolist.user.controller;
 
 import br.com.pablotzeliks.todolist.user.model.User;
+import br.com.pablotzeliks.todolist.user.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    // Endpoint to create a new user
+    @Autowired
+    private IUserRepository repository;
 
     @PostMapping("/create")
-    public void create(@RequestBody User user) {
+    public User create(@RequestBody User user) {
 
-        System.out.println("Creating user: " + user.getUsername());
+        return this.repository.save(user);
     }
 }
---
+++ src/main/java/br/com/pablotzeliks/todolist/user/repository/IUserRepository.java
@@ -0,0 +1,10 @@
+package br.com.pablotzeliks.todolist.user.repository;
+
+import br.com.pablotzeliks.todolist.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface IUserRepository extends JpaRepository<User, UUID> {
+
+}
