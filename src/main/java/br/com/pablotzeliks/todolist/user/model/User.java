2956d7887becca47401ba4794cd498b2c474121a
PabloTzeliks <arq.pabloo@gmail.com>
2025-11-22 15:21:53 -0300

    feat: implement the initial JPA structure for the User entity

--- src/main/java/br/com/pablotzeliks/todolist/user/model/User.java
+++ src/main/java/br/com/pablotzeliks/todolist/user/model/User.java
@@ -1,9 +1,19 @@
 package br.com.pablotzeliks.todolist.user.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
+@Entity(name ="tb_user")
 public class User {
+
+    @Id
+    @GeneratedValue(generator ="UUID")
+    private UUID id;
 
     private String name;
     private String username;
