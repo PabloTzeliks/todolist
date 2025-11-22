2104ac97ed78c96979f374cf2ac46204720a16ea
PabloTzeliks <arq.pabloo@gmail.com>
2025-11-22 14:43:10 -0300

    feat: add lombok, spring jpa and h2 dependencies

--- src/main/java/br/com/pablotzeliks/todolist/user/model/User.java
+++ src/main/java/br/com/pablotzeliks/todolist/user/model/User.java
@@ -1,38 +1,11 @@
 package br.com.pablotzeliks.todolist.user.model;
 
+import lombok.Data;
+
+@Data
 public class User {
 
-    private String username;
     private String name;
+    private String username;
     private String password;
-
-    public User(String username, String name, String password) {
-        this.username = username;
-        this.name = name;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
