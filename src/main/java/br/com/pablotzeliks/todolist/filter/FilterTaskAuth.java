dd53679ef2ad3e82995f0b912245a1247de7d97f
PabloTzeliks <arq.pabloo@gmail.com>
2025-11-23 01:33:55 -0300

    featÃ: add a Filter class '@OncePerRequestFilter'

---
+++ src/main/java/br/com/pablotzeliks/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,19 @@
+package br.com.pablotzeliks.todolist.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        filterChain.doFilter(request, response);
+    }
+}
