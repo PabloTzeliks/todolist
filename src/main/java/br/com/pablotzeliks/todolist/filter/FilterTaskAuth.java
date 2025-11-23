99b71011c41cbcaa394d0290a1bb7f2b48700ff6
PabloTzeliks <arq.pabloo@gmail.com>
2025-11-23 02:08:25 -0300

    feat: create a authorized route for task creation, now the system validates if you are a User

--- src/main/java/br/com/pablotzeliks/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/pablotzeliks/todolist/filter/FilterTaskAuth.java
@@ -1,19 +1,62 @@
 package br.com.pablotzeliks.todolist.filter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.pablotzeliks.todolist.user.repository.IUserRepository;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        filterChain.doFilter(request, response);
+        var servlePath = request.getServletPath();
+
+        if (!servlePath.equals("/tasks/create")) {
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        var auth = request.getHeader("Authorization");
+        var authEncoded = auth.substring("Basic".length()).trim();
+        var authDecoded = new String(Base64.getDecoder().decode(authEncoded));
+
+        String[] credentials = authDecoded.split(":");
+
+        var username = credentials[0];
+        var password = credentials[1];
+
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+
+        var user = userRepository.findByUsername(username);
+
+        if (user == null) {
+
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized User");
+        } else {
+
+            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+
+            if (!passwordVerify.verified) {
+
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized User");
+                return;
+            }
+
+            filterChain.doFilter(request, response);
+        }
     }
 }
