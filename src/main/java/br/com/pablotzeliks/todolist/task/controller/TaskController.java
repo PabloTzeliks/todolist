7558bf4454a844bf6c895192b7d74f685f42efe0
PabloTzeliks <arq.pabloo@gmail.com>
2025-11-24 21:55:45 -0300

    feat: initialize implementation of the Data validation

--- src/main/java/br/com/pablotzeliks/todolist/task/controller/TaskController.java
+++ src/main/java/br/com/pablotzeliks/todolist/task/controller/TaskController.java
@@ -4,11 +4,14 @@ import br.com.pablotzeliks.todolist.task.model.Task;
 import br.com.pablotzeliks.todolist.task.repository.ITaskRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
@@ -19,10 +22,19 @@ public class TaskController {
     private ITaskRepository repository;
 
     @PostMapping("/create")
-    public Task create(@RequestBody Task task, HttpServletRequest request) {
+    public ResponseEntity create(@RequestBody Task task, HttpServletRequest request) {
 
         task.setUserId((UUID) request.getAttribute("userId"));
 
-        return repository.save(task);
+        var currentDate = LocalDateTime.now();
+
+        if (currentDate.isBefore(task.getCreatedAt())) {
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data inicial inválida.");
+        }
+
+        var newTask = repository.save(task);
+
+        return ResponseEntity.status(HttpStatus.OK).body(newTask);
     }
 }
