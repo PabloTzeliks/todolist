e8eceb0cce235219077da5cf8f8f477c1b2a9b7e
PabloTzeliks <arq.pabloo@gmail.com>
2025-12-02 15:44:23 -0300

    feat: add DTOs for Task request and response

--- src/main/java/br/com/pablotzeliks/todolist/common/utils/Utils.java
+++ src/main/java/br/com/pablotzeliks/todolist/common/utils/Utils.java
@@ -1,4 +1,4 @@
-package br.com.pablotzeliks.todolist.utils;
+package br.com.pablotzeliks.todolist.common.utils;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeanWrapper;
