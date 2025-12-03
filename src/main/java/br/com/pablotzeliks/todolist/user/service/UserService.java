61c53bd8fa18aa5d3e86adac836d87af409e1a8d
PabloTzeliks <arq.pabloo@gmail.com>
2025-12-03 16:31:40 -0300

    feat: create the UserService with the 'create' feature, refactoring the UserController, using the Service as bridge

--- src/main/java/br/com/pablotzeliks/todolist/user/service/UserService.java
+++ src/main/java/br/com/pablotzeliks/todolist/user/service/UserService.java
@@ -1,7 +1,46 @@
 package br.com.pablotzeliks.todolist.user.service;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.pablotzeliks.todolist.user.dto.UserRequestDTO;
+import br.com.pablotzeliks.todolist.user.dto.UserResponseDTO;
+import br.com.pablotzeliks.todolist.user.mapper.UserMapper;
+import br.com.pablotzeliks.todolist.user.model.User;
+import br.com.pablotzeliks.todolist.user.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private IUserRepository repository;
+
+    @Autowired
+    private UserMapper mapper;
+
+    public UserResponseDTO create(UserRequestDTO requestDTO) {
+
+        // Validates the no existency of the User
+        var userAlreadyExists = repository.findByUsername(requestDTO.username());
+
+        if (userAlreadyExists != null) {
+
+            throw new IllegalArgumentException("Username já existente.");
+        }
+
+        // requestDTO -> Entity
+        User user = mapper.toEntity(requestDTO);
+
+        // Hashes the User Password
+        var passwordHashed = BCrypt.withDefaults()
+                .hashToString(12, requestDTO.password().toCharArray());
+
+        user.setPassword(passwordHashed);
+
+        // Persistency
+        User persistencyUser = repository.save(user);
+
+        // Entity -> responseDTO
+        return mapper.toResponse(persistencyUser);
+    }
 }
