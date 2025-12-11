2d0ad7059c4b8142f58f53f175960ca20a164eed
PabloTzeliks <arq.pabloo@gmail.com>
2025-12-11 17:58:29 -0300

    chore: clean up imports and unusual classes

--- src/main/java/br/com/pablotzeliks/todolist/user/model/User.java
+++ src/main/java/br/com/pablotzeliks/todolist/user/model/User.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
