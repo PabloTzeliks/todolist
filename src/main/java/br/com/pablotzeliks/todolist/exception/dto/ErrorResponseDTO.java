b884064d1bb9a68831096c58f957c390cd093fe1
PabloTzeliks <arq.pabloo@gmail.com>
2025-12-11 16:42:59 -0300

    feat: add DTO error for atribute validators, returning on Exception a personalized list of Field errors

--- src/main/java/br/com/pablotzeliks/todolist/exception/dto/ErrorResponseDTO.java
+++ src/main/java/br/com/pablotzeliks/todolist/exception/dto/ErrorResponseDTO.java
@@ -1,8 +1,26 @@
 package br.com.pablotzeliks.todolist.exception.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.List;
+
 public record ErrorResponseDTO(
 
         String message,
         int status,
-        String statusError
-) { }
+        String statusError,
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        List<ValidationErrorDTO> errors
+) {
+
+    public ErrorResponseDTO(String message, int status, String statusError) {
+
+        this (
+                message,
+                status,
+                statusError,
+                null
+        );
+    }
+}
