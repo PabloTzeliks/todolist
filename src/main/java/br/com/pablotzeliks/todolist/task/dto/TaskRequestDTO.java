57b1c985016a8b638a7e3c47eb6efc698407ddb4
PabloTzeliks <arq.pabloo@gmail.com>
2025-12-11 16:03:28 -0300

    feat: add atribute validators on Request DTOs

--- src/main/java/br/com/pablotzeliks/todolist/task/dto/TaskRequestDTO.java
+++ src/main/java/br/com/pablotzeliks/todolist/task/dto/TaskRequestDTO.java
@@ -1,14 +1,26 @@
 package br.com.pablotzeliks.todolist.task.dto;
 
 import br.com.pablotzeliks.todolist.task.model.Priority;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDateTime;
 
 public record TaskRequestDTO(
 
+        @NotBlank(message = "You must insert a title")
+        @Size(max = 50, message = "Title must not pass from 50 characters")
         String title,
+
+        @Size(max = 255, message = "Description must not pass from 255 characters")
         String description,
+
+        @NotNull(message = "You must insert a start date")
+        @FutureOrPresent(message = "Start date must not be from past only from present or past")
         LocalDateTime startAt,
+
+        @NotNull(message = "You must insert an end date")
+        @Future(message = "End date must not be from past or present only from future")
         LocalDateTime endAt,
+
         Priority priority
 ) { }
