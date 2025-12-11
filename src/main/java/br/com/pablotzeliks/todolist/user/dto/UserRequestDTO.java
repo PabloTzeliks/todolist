d778134620f809f67fc537f6e1d53fc4cc392ded
PabloTzeliks <arq.pabloo@gmail.com>
2025-12-11 16:03:28 -0300

    feat: add atribute validators on Request DTOs

--- src/main/java/br/com/pablotzeliks/todolist/user/dto/UserRequestDTO.java
+++ src/main/java/br/com/pablotzeliks/todolist/user/dto/UserRequestDTO.java
@@ -1,8 +1,17 @@
 package br.com.pablotzeliks.todolist.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequestDTO(
 
+        @NotBlank(message = "You must insert a name")
         String name,
+
+        @NotBlank(message = "You must insert a username")
         String username,
+
+        @NotBlank(message = "You must insert a Password")
+        @Size(min = 6, max = 20, message = "The password must have between 6 and 20 characters")
         String password
 ) { }
