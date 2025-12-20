1a274a294946cdbeffb23ea80aef17870321fdce
PabloTzeliks <arq.pabloo@gmail.com>
2025-12-20 20:23:34 -0300

    feat: add new Task listing Test on 'TaskServiceTest'

--- src/test/java/br/com/pablotzeliks/todolist/task/service/TaskServiceTest.java
+++ src/test/java/br/com/pablotzeliks/todolist/task/service/TaskServiceTest.java
@@ -14,13 +14,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TaskServiceTest {
@@ -43,49 +43,12 @@ class TaskServiceTest {
         // Arrange
         UUID userId = UUID.randomUUID();
 
-        // Create a TaskRequestDTO with test data
-        TaskRequestDTO requestDTO = new TaskRequestDTO(
-                "Test task",
-                "This is a test task",
-                LocalDateTime.now().plusDays(1),
-                LocalDateTime.now().plusDays(2),
-                Priority.HIGH
-        );
-
-        // Create a Task entity that simulates the entity before saving
-        Task taskEntity = new Task();
-        taskEntity.setUserId(userId);
-        taskEntity.setTitle(requestDTO.title());
-        taskEntity.setDescription(requestDTO.description());
-        taskEntity.setStartAt(requestDTO.startAt());
-        taskEntity.setEndAt(requestDTO.endAt());
-        taskEntity.setPriority(requestDTO.priority());
-
-        // Create a Task object that simulates the saved entity
-        Task taskSaved = new Task();
-        taskSaved.setId(UUID.randomUUID());
-        taskSaved.setUserId(userId);
-        taskSaved.setTitle(requestDTO.title());
-        taskSaved.setDescription(requestDTO.description());
-        taskSaved.setStartAt(requestDTO.startAt());
-        taskSaved.setEndAt(requestDTO.endAt());
-        taskSaved.setPriority(requestDTO.priority());
-
-        // Create a Task response DTO that simulates the expected response
-        TaskResponseDTO expectedResponse = new TaskResponseDTO(
-                taskSaved.getId(),
-                taskSaved.getTitle(),
-                taskSaved.getDescription(),
-                taskSaved.getStartAt(),
-                taskSaved.getEndAt(),
-                taskSaved.getPriority(),
-                userId,
-                taskSaved.getCreatedAt(),
-                taskSaved.getUpdatedAt()
-        );
+        TaskRequestDTO requestDTO = createValidRequest();
+        Task taskSaved = createSavedTask(userId, requestDTO);
+        TaskResponseDTO expectedResponse = createResponse(taskSaved);
 
         // Mocking the mapper behavior from RequestDTO to Entity
-        when(taskMapper.toEntity(any(TaskRequestDTO.class))).thenReturn(taskEntity);
+        when(taskMapper.toEntity(any(TaskRequestDTO.class))).thenReturn(new Task());
 
         // Mocking the repository save method
         when(taskRepository.save(any(Task.class))).thenReturn(taskSaved);
@@ -102,7 +65,7 @@ class TaskServiceTest {
         assertEquals(taskSaved.getId(), responseDTO.id());
         assertEquals(taskSaved.getUserId(), responseDTO.userId());
         assertEquals("Test task", responseDTO.title());
-        assertEquals("This is a test task", responseDTO.description());
+        assertEquals("Description", responseDTO.description());
         assertEquals(taskSaved.getStartAt(), responseDTO.startAt());
         assertEquals(taskSaved.getEndAt(), responseDTO.endAt());
         assertEquals(Priority.HIGH, responseDTO.priority());
@@ -111,5 +74,80 @@ class TaskServiceTest {
         verify(taskMapper).toEntity(any(TaskRequestDTO.class));
         verify(taskRepository).save(any(Task.class));
         verify(taskMapper).toResponse(any(Task.class));
+    }
+
+    @Test
+    @DisplayName("Test if TaskService is correctly listing Tasks. Should successfully list the Tasks when everything is correct.")
+    void listTaskSuccessfully_Test() {
+
+        // Triple A pattern: Arrange, Act, Assert
+
+        // Arrange
+        var userId = UUID.randomUUID();
+
+        TaskRequestDTO firstTaskRequest = createValidRequest();
+        TaskRequestDTO secondTaskRequest = createValidRequest();
+
+        Task firstTaskSaved = createSavedTask(userId, firstTaskRequest);
+        Task secondTaskSaved = createSavedTask(userId, secondTaskRequest);
+
+        List<Task> taskFromDB = List.of(firstTaskSaved, secondTaskSaved);
+
+        TaskResponseDTO firstExpectedResponse = createResponse(firstTaskSaved);
+        TaskResponseDTO secondExpectedResponse = createResponse(secondTaskSaved);
+
+        // Mocking the repository findByUserId method
+        when(taskRepository.findByUserId(userId)).thenReturn(taskFromDB);
+
+        // Mocking the mapper behavior from Entity to ResponseDTO
+        when(taskMapper.toResponse(firstTaskSaved)).thenReturn(firstExpectedResponse);
+        when(taskMapper.toResponse(secondTaskSaved)).thenReturn(secondExpectedResponse);
+
+        // Act
+        List<TaskResponseDTO> resultList = taskService.list(userId);
+
+        // Assert
+        assertNotNull(resultList);
+        assertEquals(2, resultList.size());
+
+        assertEquals(firstExpectedResponse.id(), resultList.get(0).id());
+        assertEquals(secondExpectedResponse.id(), resultList.get(1).id());
+
+        // Verify if all the Methods were called as expected
+        verify(taskRepository).findByUserId(userId);
+
+        verify(taskMapper, times(1)).toResponse(firstTaskSaved);
+        verify(taskMapper, times(1)).toResponse(secondTaskSaved);
+    }
+
+    // Auxiliary methods
+
+    private TaskRequestDTO createValidRequest() {
+        return new TaskRequestDTO(
+                "Test task", "Description",
+                LocalDateTime.now(), LocalDateTime.now().plusDays(1),
+                Priority.HIGH
+        );
+    }
+
+    private Task createSavedTask(UUID userId, TaskRequestDTO requestDTO) {
+        Task taskSaved = new Task();
+        taskSaved.setId(UUID.randomUUID());
+        taskSaved.setUserId(userId);
+        taskSaved.setTitle(requestDTO.title());
+        taskSaved.setDescription(requestDTO.description());
+        taskSaved.setStartAt(requestDTO.startAt());
+        taskSaved.setEndAt(requestDTO.endAt());
+        taskSaved.setPriority(requestDTO.priority());
+
+        return taskSaved;
+    }
+
+    private TaskResponseDTO createResponse(Task task) {
+        return new TaskResponseDTO(
+                task.getId(), task.getTitle(), task.getDescription(),
+                task.getStartAt(), task.getEndAt(), task.getPriority(),
+                task.getUserId(), task.getCreatedAt(), task.getUpdatedAt()
+        );
     }
 }
